f94e7aa0a6eb551d4a6cdd055d5a0f7eeb360788
Paul Sims <paul.q.sims@gmail.com>
2020-08-20 16:05:55 -0400

    Update render file for just the summary output for now

--- R/render-Rmd.R
+++ R/render-Rmd.R
@@ -18,9 +18,13 @@ docs_to_render <-
   c("R/analysis_repeatability.Rmd", "R/analysis_predictbehav.Rmd",
     "R/figures.Rmd", "R/analysis_summary.Rmd") 
 
-for (i in seq_along(docs_to_render)) {
-  rmarkdown::render(docs_to_render[i], output_dir = "reports/",
-                    output_format = c("html_document",
-                                      "github_document"))
-}
+# for (i in seq_along(docs_to_render)) {
+#   rmarkdown::render(docs_to_render[i], output_dir = "reports/",
+#                     output_format = c("html_document",
+#                                       "github_document"))
+# }
 
+# Render just the summary file of the analyses
+rmarkdown::render("R/analysis_summary.Rmd",
+                  output_dir = "reports/",
+                  output_format = c("html_document", "github_document"))
