4a63cf2657380bd5f388045b9aad744f55d57c96
Paul Sims <paul.q.sims@gmail.com>
2020-08-26 14:36:39 -0400

    Make for loop more readable and change output to .rmd self-contained

--- R/render-Rmd.R
+++ R/render-Rmd.R
@@ -18,7 +18,7 @@ rmarkdown::render("readme.Rmd",
 docs_to_render <-
   c("R/analysis_summary.Rmd") # "R/analysis_repeatability.Rmd", "R/analysis_predictbehav.Rmd", "R/figures.Rmd", 
 
-for (i in seq_along(docs_to_render)) {
-  rmarkdown::render(docs_to_render[i], output_dir = "reports/",
-                    output_format = "all")
+for (doc in seq_along(docs_to_render)) {
+    rmarkdown::render(docs_to_render[i], output_dir = "reports/",
+                      output_format = NULL)
 }
